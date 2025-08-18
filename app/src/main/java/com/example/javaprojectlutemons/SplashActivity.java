8fef2587fa47826847467c218f74d03f1c4d95e6
avaneepk <119371288+avaneepk@users.noreply.github.com>
2025-08-18 22:38:06 +0300

    changed the java files to move to homepage

--- app/src/main/java/com/example/javaprojectlutemons/SplashActivity.java
+++ app/src/main/java/com/example/javaprojectlutemons/SplashActivity.java
@@ -1,20 +1,30 @@
-package com.example.lutemon;
+package com.example.javaprojectlutemons;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Simulate brief splash then go to Loading
-        new Handler().postDelayed(() ->
-                        startActivity(new Intent(this, LoadingActivity.class))
-                , 1200);
+        timer = new Timer();
+        timer.schedule(new TimerTask(){
+           @Override
+           public void run() {
+               Intent intent = new Intent(SplashActivity.this, com.example.javaprojectlutemons.LoadingActivity.class);
+                startActivity(intent);
+                finish();
+           }
+        }, 3000);
+
     }
 }
