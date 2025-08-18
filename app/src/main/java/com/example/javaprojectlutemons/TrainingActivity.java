803c49ab091623667cdaac8fefab157e8d54d447
avaneepk <119371288+avaneepk@users.noreply.github.com>
2025-08-18 23:18:37 +0300

    clearing sudo code

--- app/src/main/java/com/example/javaprojectlutemons/TrainingActivity.java
+++ app/src/main/java/com/example/javaprojectlutemons/TrainingActivity.java
@@ -8,28 +8,5 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class TrainingActivity extends AppCompatActivity {
 
-    @Override protected void onCreate(Bundle b) {
-        super.onCreate(b);
-        setContentView(R.layout.activity_training);
 
-        BottomNavigationView nav = findViewById(R.id.bottom_nav);
-        nav.setSelectedItemId(R.id.nav_home);
-        nav.setOnItemSelectedListener(this::onNav);
-
-        findViewById(R.id.btn_train).setOnClickListener(v -> {
-            // TODO training logic
-        });
-        findViewById(R.id.btn_potions).setOnClickListener(v -> {
-            // TODO choose potions
-        });
-    }
-
-    private boolean onNav(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_back:     finish(); return true;
-            case R.id.nav_home:     startActivity(new Intent(this, HomeActivity.class)); return true;
-            case R.id.nav_settings: startActivity(new Intent(this, SettingsActivity.class)); return true;
-            default: return false;
-        }
-    }
 }
